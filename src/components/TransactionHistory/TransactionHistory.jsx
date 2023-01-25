6dbf888ffe496d8c2bb9437a564099acff99dabf
Yuruy Vytyaganets <yvytyawork@gmail.com>
2023-01-25 21:39:49 +0200

    commit

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,4 +1,5 @@
 import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
 const TransactionHistory = ({ items }) => {
   return (
     <table className={css.transactionHistory}>
@@ -25,3 +26,13 @@ const TransactionHistory = ({ items }) => {
 };
 
 export default TransactionHistory;
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
