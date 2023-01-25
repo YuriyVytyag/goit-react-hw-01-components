f40c3b59a70891bae3b69de74b45763abd900be5
Yuruy Vytyaganets <yvytyawork@gmail.com>
2023-01-25 17:54:05 +0200

    status styles

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,7 +3,17 @@ import PropTypes from 'prop-types';
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      <span className={css.status}>{isOnline}</span>
+      <span
+        className={css.status}
+        style={{
+          borderRadius: '50%',
+          display: 'block',
+          width: '15px',
+          height: '15px',
+          backgroundColor: isOnline ? 'green' : 'red',
+        }}
+      />
+
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
